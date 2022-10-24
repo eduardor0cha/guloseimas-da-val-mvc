30007464e3cb7ff77145f68ebe81ac3e4a42718e
guilherme-gjv <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>
2022-10-24 12:22:44 -0300

    css e páginas configurados

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const { engine } = require("express-handlebars")
+const path = require("path")
 const app = express()
 
     // configuração para converter o body da requisição para json (body-parser descontinuado)
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/views')))
 app.use(express.urlencoded({ extended: true }));
 
     // template engine handlebars
 app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars'); 
 
+// ROTAS
 app.get('/', (req, res) => {
     res.render('index')
 })
+
+app.get('/home', (req, res) => {
+    res.render('home')
+})
+
+
+// RODAR SERVIDOR
 
 const PORT = 9130
 
