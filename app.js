e330a3baf833453e640ba90c2b135f986b326663
guilherme-gjv <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>
2022-10-24 12:01:26 -0300

    handlebars instalado e configurado

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const { engine } = require("express-handlebars")
 const app = express()
 
     // configuração para converter o body da requisição para json (body-parser descontinuado)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+    // template engine handlebars
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars'); 
+
 app.get('/', (req, res) => {
-    res.send("Guloseimas da Val");
+    res.render('index')
 })
 
 const PORT = 9130
