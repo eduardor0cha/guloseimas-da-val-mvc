f59755c638b2b53a380afdf5d5e9f9badf5eead8
guilherme-gjv <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>
2022-10-24 12:00:59 -0300

    body parse to json

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express()
 
+    // configuração para converter o body da requisição para json (body-parser descontinuado)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send("Guloseimas da Val");
 })
