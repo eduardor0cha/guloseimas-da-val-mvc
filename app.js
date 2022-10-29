0d6b70509d0449e98830c2c8a099b22b752bdae6
guilherme-gjv <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>
2022-10-29 13:14:11 -0300

    função que gera arquivo de configuração de banco.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const administrador = require("./routes/administrador");
 const usuario = require("./routes/usuario");
 const mongoose = require("mongoose");
+gerarArquivoDeBanco();
 const banco = require("./config/banco");
 
 /* configuração para converter o body da requisição para json (body-parser descontinuado) */
@@ -45,6 +46,27 @@ app.get("/home", (req, res) => {
 
 app.use("/admin", administrador);
 app.use("/user", usuario);
+
+/* Função que gera arquivo de configuração de banco */
+function gerarArquivoDeBanco() {
+  const fs = require("fs");
+  const caminho = "./config/banco.js";
+
+  if (fs.existsSync(caminho)) {
+    console.log("O arquivo banco.js existe! :)");
+  } else {
+    fs.writeFile(caminho, "", (erro) => {
+      if (!erro) {
+        console.log(
+          "Arquivo banco.js criado! Leia o arquivo ./config/banco.exemplo.js :)"
+        );
+      } else {
+        console.log("Não foi possível criar banco.js: " + erro);
+      }
+    });
+  }
+}
+
 /* RODAR SERVIDOR */
 const PORT = 9130;
 
