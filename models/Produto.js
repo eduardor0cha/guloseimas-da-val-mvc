95fd06a3ed53207ec92e8b7b850727c8bbfd052b
guilherme-gjv <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>
2022-10-29 09:20:23 -0300

    fix: model mongoose estava com o nome errado!!

--- models/Produto.js
+++ models/Produto.js
@@ -2,34 +2,34 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Produto = new Schema({
-    nome:{
-        type: String,
-        required:true
-    },
-    descricao:{
-        type: String,
-        required:true
-    },
-    sabor:{
-        type: String,
-        required: true
-    },
-    preco:{
-        type: Number,
-        required: true
-    },
-    estoque: {
-        type: Number,
-        required: true
-    },
-    exibirAoCliente: {
-        type: Schema.Types.boolean,
-        default: false
-    },
-    dataDeCriacao: {
-        type: Date,
-        default: Date.now()
-    }
-})
+  nome: {
+    type: String,
+    required: true,
+  },
+  descricao: {
+    type: String,
+    required: true,
+  },
+  sabor: {
+    type: String,
+    required: true,
+  },
+  preco: {
+    type: Number,
+    required: true,
+  },
+  estoque: {
+    type: Number,
+    required: true,
+  },
+  exibirAoCliente: {
+    type: Schema.Types.boolean,
+    default: false,
+  },
+  dataDeCriacao: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
-mongoose.model("postagens",Postagem)
+mongoose.model("produtos", Produto);
