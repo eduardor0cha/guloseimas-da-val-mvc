b5bdd2e214fa0b93c3ddaf287b4031f013cc51b5
guilherme-gjv <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>
2022-10-29 08:26:15 -0300

    fix: código bonito :)

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 const express = require("express");
-const { engine } = require("express-handlebars")
-const path = require("path")
-const app = express()
+const { engine } = require("express-handlebars");
+const path = require("path");
+const app = express();
 
-    // configuração para converter o body da requisição para json (body-parser descontinuado)
+/* configuração para converter o body da requisição para json (body-parser descontinuado) */
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/views')))
+app.use(express.static(path.join(__dirname, "/views")));
 app.use(express.urlencoded({ extended: true }));
 
-    // template engine handlebars
-app.engine('handlebars', engine({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars'); 
+/* template engine handlebars */
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// ROTAS
-app.get('/', (req, res) => {
-    res.render('index')
-})
+/* ROTAS */
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/home', (req, res) => {
-    res.render('home')
-})
+app.get("/home", (req, res) => {
+  res.render("home");
+});
 
+/* RODAR SERVIDOR */
+const PORT = 9130;
 
-// RODAR SERVIDOR
-
-const PORT = 9130
-
-app.listen(PORT, ()=>{
-    console.log('O servidor está rodando na porta ' + PORT);
+app.listen(PORT, () => {
+  console.log("O servidor está rodando na porta " + PORT);
 });
