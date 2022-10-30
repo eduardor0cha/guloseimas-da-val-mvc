f8e76c9e4f3233b4333ffa670ffc7ecd58ac1ff3
guilherme-gjv <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>
2022-10-30 18:05:02 -0300

    por segurança e semântica, use const já que o valor não muda

--- routes/administrador.js
+++ routes/administrador.js
@@ -98,7 +98,7 @@ route.post("/produtos/editar", (req, res) => {
 });
 
 route.post("/produtos/criar", (req, res) => {
-  var produto = new Produto({
+  const produto = new Produto({
     nome: req.body.nome,
     descricao: req.body.descricao,
     sabor: req.body.sabor,
